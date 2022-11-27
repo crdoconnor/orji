c782ef0292f6049e8987072b496cbf5a8b95b5f2
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-11-27 21:11:48 +0000

    More features.

--- src/orji/note.py
+++ src/orji/note.py
@@ -1,9 +1,20 @@
 from slugify import slugify
 
-
-class Body:
+class Line:
     def __init__(self, text):
         self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+class Body:
+    def __init__(self, node):
+        self.text = node.body
+    
+    @property
+    def lines(self):
+        return [Line(line_text) for line_text in self.text.split("\n")]
     
     def __str__(self):
         return self.text
@@ -26,8 +37,12 @@ class Note:
         return self._node.todo
 
     @property
+    def tags(self):
+        return self._node.tags
+
+    @property
     def body(self):
-        return Body(self._node.body)
+        return Body(self._node)
 
     def __iter__(self):
         for node in self._node.children:
