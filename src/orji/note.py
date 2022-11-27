ab0c6eb0c2105c82140f9c1668eb4773cfc15a1e
Colm O'Connor <colm.oconnor.github@gmail.com>
2022-11-27 21:58:17 +0000

    Added properties.

--- src/orji/note.py
+++ src/orji/note.py
@@ -1,5 +1,6 @@
 from slugify import slugify
 
+
 class Line:
     def __init__(self, text):
         self.text = text
@@ -11,11 +12,11 @@ class Line:
 class Body:
     def __init__(self, node):
         self.text = node.body
-    
+
     @property
     def lines(self):
         return [Line(line_text) for line_text in self.text.split("\n")]
-    
+
     def __str__(self):
         return self.text
 
@@ -27,11 +28,11 @@ class Note:
     @property
     def name(self):
         return self._node.heading
-    
+
     @property
     def slug(self):
         return slugify(self._node.heading)
-    
+
     @property
     def state(self):
         return self._node.todo
@@ -43,15 +44,21 @@ class Note:
     @property
     def body(self):
         return Body(self._node)
-    
+
+    @property
+    def prop(self):
+        return self._node.properties
+
     def at(self, lookup):
         matching_notes = [n for n in self._node.children if n.heading == lookup]
         if len(matching_notes) == 0:
             raise Exception(f"No notes found in {self.heading} with name {lookup}")
         elif len(matching_notes) > 1:
-            raise Exception(f"More than one notes found in {self.heading} with name {lookup}")
+            raise Exception(
+                f"More than one notes found in {self.heading} with name {lookup}"
+            )
         else:
-            return matching_notes[0]
+            return Note(matching_notes[0])
 
     def __iter__(self):
         for node in self._node.children:
