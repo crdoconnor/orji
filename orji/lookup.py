08bb01ce40f52cff3c716d6532fb18aab5b5a1af
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-12-25 09:54:26 +0000

    REFACTOR : Satisfy linter.

--- orji/lookup.py
+++ orji/lookup.py
@@ -1,10 +1,12 @@
 import orgmunge
 from .note import Note
+from pathlib import Path
 
 
 class LookupItem:
     def __init__(self, item):
         self.index = int(item)
+
 
 class Lookup:
     def __init__(self, text):
@@ -25,14 +27,14 @@ class Lookup:
     def full(self):
         return True
 
-    def load(self):
+    def load(self, temp_dir):
+        org_text = Path(self.filepath).read_text()
         munge_parsed = orgmunge.Org(
             org_text,
             from_file=False,
             todos={"todo_states": {"todo": "TODO"}, "done_states": {"done": "DONE"}},
         )
-        notes = Note(munge_parsed.root, temp_dir=temp_dir)
-        current_note = None
+        current_note = Note(munge_parsed.root, temp_dir=temp_dir)
         for item in self.parsed_ref:
             current_note = current_note.children[item.index]
         return current_note
