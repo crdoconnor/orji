c644c6be506e1ea771091f5df7a5936264cd2db2
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-12-25 10:24:44 +0000

    REFACTOR : Consolidate temporary directory code.

--- orji/tempdir.py
+++ orji/tempdir.py
@@ -1,7 +1,18 @@
 import os
-from .utils import random_5_digit_number
 from pathlib import Path
 import shutil
+import random
+
+MOCK_RANDOM_FIX = 11110
+
+
+def random_5_digit_number():
+    if os.getenv("MOCK") == "yes":
+        global MOCK_RANDOM_FIX
+        MOCK_RANDOM_FIX = MOCK_RANDOM_FIX + 1
+        return MOCK_RANDOM_FIX
+    else:
+        return random.randint(10000, 99999)
 
 
 class TempDir:
