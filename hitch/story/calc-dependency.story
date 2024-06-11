e8b2d737198f8758228ec4ac68aa26720c71d9ac
Colm O'Connor <colm.oconnor.github@gmail.com>
2024-06-11 08:01:22 +0100

    FEATURE : Calculations with child dependencies.

--- hitch/story/calc-dependency.story
+++ hitch/story/calc-dependency.story
@@ -1,30 +1,30 @@
-#Calculation with dependency:
-  #docs: basic-calculation
-  #about: |
-    #Run a calculation from within a note.
-  #variations:
-    #First run:
-      #given:
-        #files:
-          #org/calc.org: |
-            #* Number of chairs = ?
-            #=100+200+number_of_chairs_in_section_b+number_of_chairs_in_section_c
-            #** Number of chairs in section b = ?
-            #150+150
-            #** Number of chairs in section c = 400
-      #steps:
-      #- orji:
-          #env:
-            #ORJITMP: ./tmp
-          #cmd: calc org/calc.org//0
-          #output: |
-            #Written note(s) successfully
+Calculation with dependency:
+  docs: basic-calculation
+  about: |
+    Run a calculation from within a note.
+  variations:
+    First run:
+      given:
+        files:
+          org/calc.org: |
+            * Number of chairs = ?
+            =100+200+number_of_chairs_in_section_b+number_of_chairs_in_section_c
+            ** Number of chairs in section b = ?
+            =150+150
+            ** Number of chairs in section c = 400
+      steps:
+      - orji:
+          env:
+            ORJITMP: ./tmp
+          cmd: calc org/calc.org//0
+          output: |
+            Written note(s) successfully
 
-      #- file contents:
-          #filename: org/calc.org
-          #contents: |
-            #* Number of chairs = ?
-            #=100+200+number_of_chairs_in_section_b+number_of_chairs_in_section_c
-            #** Number of chairs in section b = ?
-            #150+150
-            #** Number of chairs in section c = 400
+      - file contents:
+          filename: org/calc.org
+          contents: |
+            * Number of chairs = 1000.0
+            =100+200+number_of_chairs_in_section_b+number_of_chairs_in_section_c
+            ** Number of chairs in section b = 300
+            =150+150
+            ** Number of chairs in section c = 400
--- orji/calculation.py
+++ orji/calculation.py
@@ -4,6 +4,7 @@ from .loader import Loader
 from .lookup import Lookup
 from pathlib import Path
 import orgmunge
+from copy import copy
 
 
 class Modification:
@@ -11,36 +12,56 @@ class Modification:
 
 
 class NewTitle(Modification):
-    def __init__(self, relative: tuple[int, ...], title):
+    def __init__(self, relative, title):
         self.relative = relative
         self.title = title
 
 
 class AddError(Modification):
-    def __init__(self, relative: tuple[int, ...], error_title, error_text):
+    def __init__(self, relative, error_title, error_text):
         self.relative = relative
         self.error_title = error_title
         self.error_text = error_text
 
 
-def perform_calculation(calc_note, modifications):
+def slugify(text, separator="-"):
+    return text.lower().strip().replace(" ", separator)
+
+
+def underscore_slugify(text):
+    """Changes "Something like this" to "something_like_this"."""
+    return slugify(text, separator="_")
+
+
+def perform_calculation(calc_note, modifications, variables):
     headline = calc_note.name
     body = calc_note.body.text
 
-    if body.startswith("=") and "=" in headline:
-        formula = body.lstrip("=")
+    if "=" in headline:
         left_hand_side = headline.split("=")[0]
 
         try:
-            actual_value = eval(formula)
-            modifications.append(NewTitle((0,), actual_value))
-            new_title = left_hand_side + "= " + str(actual_value)
-            if new_title != calc_note.name:
-                modifications.append(NewTitle((0,), new_title))
-        except Exception as error:
-            modifications.append(
-                AddError((0,), type(error).__name__.strip(), str(error))
-            )
+            actual_value = float(headline.split("=")[1])
+        except ValueError:
+            actual_value = None
+
+        if body.startswith("="):
+            formula = body.lstrip("=")
+
+            try:
+                actual_value = eval(formula, copy(variables))
+                modifications.append(NewTitle(calc_note, actual_value))
+                new_title = left_hand_side + "= " + str(actual_value)
+                if new_title != calc_note.name:
+                    modifications.append(NewTitle(calc_note, new_title))
+            except Exception as error:
+                modifications.append(
+                    AddError(calc_note, type(error).__name__.strip(), str(error))
+                )
+
+        variables[underscore_slugify(left_hand_side)] = (
+            float(actual_value) if actual_value is not None else None
+        )
 
 
 @click.command()
@@ -52,20 +73,29 @@ def calculation(relative):
     lookup = Lookup(relative)
 
     modifications: Modification = []
+    variables = {}
 
     calc_note = lookup.load(loader)
-    perform_calculation(calc_note, modifications)
+
+    children = calc_note.children
+    children.reverse()
+
+    for child in children:
+        perform_calculation(child, modifications, variables)
+
+    perform_calculation(calc_note, modifications, variables)
 
     for modification in modifications:
+        modify_note = modification.relative
         children = [
             node
-            for node in calc_note._node.children
+            for node in modify_note._node.children
             if node.tags is None or "calcerror" not in node.tags
         ]
 
         if isinstance(modification, NewTitle):
-            calc_note.set_name(modification.title)
-            calc_note._node.children = children
+            modify_note.set_name(modification.title)
+            modify_note._node.children = children
         elif isinstance(modification, AddError):
             chunk_to_insert = orgmunge.Org(
                 f"* {modification.error_title} :calcerror:\n{modification.error_text}",
@@ -76,8 +106,8 @@ def calculation(relative):
                 },
             ).root.children[0]
             children = list(children)
-            chunk_to_insert.sibling = calc_note._node
-            calc_note._node.children = children
+            chunk_to_insert.sibling = modify_note._node
+            modify_note._node.children = children
             chunk_to_insert.demote()
         else:
             raise NotImplementedError
