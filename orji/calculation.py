39b60e76127ca7eb08c8c5edbeacd7d3b4e3c044
Colm O'Connor <colm.oconnor.github@gmail.com>
2024-06-11 06:53:40 +0100

    REFACTOR : Cleaned up calculation.py.

--- orji/calculation.py
+++ orji/calculation.py
@@ -23,6 +23,26 @@ class AddError(Modification):
         self.error_text = error_text
 
 
+def perform_calculation(calc_note, modifications):
+    headline = calc_note.name
+    body = calc_note.body.text
+
+    if body.startswith("=") and "=" in headline:
+        formula = body.lstrip("=")
+        left_hand_side = headline.split("=")[0]
+
+        try:
+            actual_value = eval(formula)
+            modifications.append(NewTitle((0,), actual_value))
+            new_title = left_hand_side + "= " + str(actual_value)
+            if new_title != calc_note.name:
+                modifications.append(NewTitle((0,), new_title))
+        except Exception as error:
+            modifications.append(
+                AddError((0,), type(error).__name__.strip(), str(error))
+            )
+
+
 @click.command()
 @click.argument("relative")
 def calculation(relative):
@@ -33,36 +53,19 @@ def calculation(relative):
 
     modifications: Modification = []
 
-    write_note = lookup.load(loader)
-
-    headline = write_note.name
-    body = write_note.body.text
-
-    if body.startswith("=") and "=" in headline:
-        formula = body.lstrip("=")
-        left_hand_side = headline.split("=")[0]
-
-        try:
-            actual_value = eval(formula)
-            modifications.append(NewTitle((0,), actual_value))
-            new_title = left_hand_side + "= " + str(actual_value)
-            if new_title != write_note.name:
-                modifications.append(NewTitle((0,), new_title))
-        except Exception as error:
-            modifications.append(
-                AddError((0,), type(error).__name__.strip(), str(error))
-            )
+    calc_note = lookup.load(loader)
+    perform_calculation(calc_note, modifications)
 
     for modification in modifications:
         children = [
             node
-            for node in write_note._node.children
+            for node in calc_note._node.children
             if node.tags is None or "calcerror" not in node.tags
         ]
 
         if isinstance(modification, NewTitle):
-            write_note.set_name(new_title)
-            write_note._node.children = children
+            calc_note.set_name(modification.title)
+            calc_note._node.children = children
         elif isinstance(modification, AddError):
             chunk_to_insert = orgmunge.Org(
                 f"* {modification.error_title} :calcerror:\n{modification.error_text}",
@@ -73,12 +76,12 @@ def calculation(relative):
                 },
             ).root.children[0]
             children = list(children)
-            chunk_to_insert.sibling = write_note._node
-            write_note._node.children = children
+            chunk_to_insert.sibling = calc_note._node
+            calc_note._node.children = children
             chunk_to_insert.demote()
         else:
             raise NotImplementedError
 
-    Path(lookup.filepath).write_text(str(write_note))
+    Path(lookup.filepath).write_text(str(calc_note))
     click.echo("Written note(s) successfully")
     temp_dir.destroy()
